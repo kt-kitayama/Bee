54880a39d2c5053469c566caec5320efa823b8bc
kt-kitayama <71867046+kt-kitayama@users.noreply.github.com>
2020-11-26 15:37:15 +0900

    update
    update

--- js/script.js
+++ js/script.js
@@ -10,23 +10,7 @@ var t_ort = new L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}
     attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>"
 });
 
-var t_flw = new L.vectorGrid.protobuf("https://kt-kitayama.github.io/BuzzMap/Flw/{z}/{x}/{y}.mvt", {
-    attribution: "<a href='https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-A45.html' target='_blank'>林野庁・秋田県</a>",
-    maxNativeZoom: 14,
-    minNativeZoom: 14,
-    maxZoom: 18,
-    rendererFactory: L.canvas.tile,
-    vectorTileLayerStyles: {
-        "AkitaBee(樹種のみ蜜源)": {
-            color: "red",
-            weight: 2
-        }
-    }
-}).addTo(map);
-
-
 var Map_b = {
-    "ふらわ": t_flw,
     "地理院地図 標準": t_std,
     "地理院地図 淡色": t_pale,
     "地理院地図 オルソ": t_ort,
@@ -41,6 +25,22 @@ var map = L.map('map', {
 L.control.scale({ maxWidth: 250, imperial: false }).addTo(map);
 
 L.control.layers(Map_b, null, { collapsed: false }).addTo(map);
+
+var t_flw = new L.vectorGrid.protobuf("https://kt-kitayama.github.io/BuzzMap/Flw/{z}/{x}/{y}.mvt", {
+    attribution: "<a href='https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-A45.html' target='_blank'>林野庁・秋田県</a>",
+    maxNativeZoom: 14,
+    minNativeZoom: 14,
+    maxZoom: 18,
+    rendererFactory: L.canvas.tile,
+    vectorTileLayerStyles: {
+        "AkitaBee(樹種のみ蜜源)": {
+            color: "red",
+            weight: 2
+        }
+    }
+});
+t_flw.bringToFront();
+t_flw.addTo(map);
 
 // singleclick（mapに設定）
 map.options.singleClickTimeout = 250;
